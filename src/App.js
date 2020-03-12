f84326789a9c779ce035261c117afca9cf5797f1
Avera Guthrie <avera@Averas-MacBook-Pro.local>
2020-03-12 14:58:12 -0500

    Added css and component for cal and back

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Backlog from './components/Backlog'
+import Calendar from './components/Calendar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       HEADER
       </header>
+      <body className='Grid-body'>
+        <div className='Calendar'>
+          <Calendar />
+        </div>
+        <div className='Backlog'>
+          <Backlog />
+        </div>
+      </body>
     </div>
   );
 }
---
+++ src/components/Backlog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Backlog.scss';
+
+function Backlog() {
+  return (
+    <div className='Back'>
+      Backlog
+    </div>
+  )
+}
+
+export default Backlog;
