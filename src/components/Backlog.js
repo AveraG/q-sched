e322ce407b966963c4a43e1fbc1cc7122e518b60
Avera Guthrie <avera@Averas-MacBook-Pro.local>
2020-03-23 13:21:46 -0500

    backlog is mocked up add/delete/edit

--- src/components/Backlog.js
+++ src/components/Backlog.js
@@ -1,12 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Backlog.scss';
+import ListItem from './ListItem';
+//import { tsPropertySignature } from '@babel/types';
+
 
 function Backlog() {
+  const [todos, setTodos] = useState([
+    {
+      content: 'Feed dog',
+      isCompleted: true,
+    },
+    {
+      content: 'Do dishes',
+      isCompleted: false,
+    },
+    {
+      content: 'Complete project',
+      isCompleted: false,
+    }
+  ]);
+
+  function handleKeyDown(e, i) {
+    if (e.key === 'Enter') {
+      createTodo(e, i);
+    }
+    if (e.key === 'Backspace' && todos[i].content === '') {
+      e.preventDefault();
+      return removeTodo(i);
+    }
+  }
+
+  function removeTodo(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
+    setTimeout(() => {
+      if(i > 0) document.forms[0].elements[i - 1].focus();
+    }, 0);
+  }
+
+  function createTodo(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: '',
+      isComlpleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+
+  function updateTodo(e, i) {
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
+
+  function toggle(i) {
+    const temporaryTodos = [...todos];
+    temporaryTodos[i].isCompleted=!temporaryTodos[i].isCompleted;
+    setTodos(temporaryTodos);
+  }
+
   return (
-    <div className='Back'>
-      Backlog
+    <div className="backlog">
+      <div className="header">
+        <h3>Backlog</h3> 
+      </div>
+      <form className="todo-list">
+        <ul>
+          {todos.map((todo, i) => (
+           <ListItem key={i} todo={todo} toggle={toggle} handleKeyDown={handleKeyDown} updateTodo={updateTodo} i={i} />
+          ))}
+        </ul>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Backlog;
---
+++ src/components/ListItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function ListItem({todo, toggle, handleKeyDown, updateTodo, i}) {
+    
+    return(
+        <div className={`todo ${todo.isComlpleted && 'todo-is-completed'}`}>
+        <div className={'checkbox'} onClick={() => toggle(i)}>
+          {todo.isCompleted && (
+            <span>&#x2714;</span>
+          )}
+        </div>
+        <input
+          type="text"
+          value={todo.content}
+          onKeyDown={e => handleKeyDown(e, i)}
+          onChange={e => updateTodo(e, i)}
+        />
+      </div>
+    )
+}
+
+export default ListItem;
