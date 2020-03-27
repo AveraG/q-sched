6786d81524b0849ce8f09cffadb9efeede12476a
Avera Guthrie <avera@Averas-MacBook-Pro.local>
2020-03-27 11:01:43 -0500

    Added in dnd dragging ability

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,8 +5,10 @@ import moment from 'moment';
 //import './month.scss';
 import './styles.scss';
 import events from '../events'
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 
 const localizer = momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(Calendar)
 
 class MyCalendar extends Component {
   constructor() {
@@ -17,17 +19,31 @@ class MyCalendar extends Component {
     };
   }
 
+  onEventResize = (type, { event, start, end, allDay }) => {
+    this.setState(state => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: state.events };
+    });
+  };
+
+  onEventDrop = ({ event, start, end, allDay }) => {
+    console.log(start);
+  };
   //component did mount to fetch events and set state [setState: events:events]
 
   render() {
     return (
-          <Calendar
-            events={this.state.events}
-            startAccessor="start"
-            endAccessor="end"
-            defaultDate={moment().toDate()}
-            localizer={localizer}
-          />
+      <DnDCalendar
+      defaultDate={moment().toDate()}
+      defaultView="month"
+      events={this.state.events}
+      localizer={localizer}
+      onEventDrop={this.onEventDrop}
+      onEventResize={this.onEventResize}
+      resizable
+      style={{ height: "100vh" }}
+    />
     );
   }
 }
