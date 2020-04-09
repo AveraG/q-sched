8487c5b0340239741f2bc66fa0ca57548c1f9631
Avera Guthrie <avera@Averas-MacBook-Pro.local>
2020-04-09 16:05:06 -0500

    added ModalButton to App.js. doesn't work

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.scss';
 import Backlog from './components/Backlog'
 import MyCalendar from './components/Calendar'
 import Header from './components/Header'
+import ModalButton from '../src/components/Modal/ModalButton'
 
 
 function App() {
   return (
     <div className="App">
        <Header />
+       <ModalButton />
       <body className='Grid-body'>
           <MyCalendar />
           <Backlog />
--- src/components/Modal/ModalButton.js
+++ src/components/Modal/ModalButton.js
@@ -1,2 +1,21 @@
 import React from 'react'
+import Modal from './Modal'
+import ToggleContent from './ToggleContent'
 
+const ModalButton = () => (
+  <p>
+    Click to reveal a secret:
+
+    <ToggleContent
+      toggle={show => <button onClick={show}>Open</button>}
+      content={hide => (
+        <Modal>
+          There is no spoon.<br/>
+          <button onClick={hide}>Close</button>
+        </Modal>
+      )}
+    />
+  </p>
+);
+
+export default ModalButton;
