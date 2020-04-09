493826d69fe578b1bfff74d0bf74d7ce3cb49878
Avera Guthrie <avera@Averas-MacBook-Pro.local>
2020-04-09 15:42:24 -0500

    broken app update

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.scss';
 import Backlog from './components/Backlog'
 import MyCalendar from './components/Calendar'
 import Header from './components/Header'
-import ReactModal from 'react-modal'
+//import ReactModal from 'react-modal'
 
 function App() {
-  const [showModal, hideModal] = useModal(() => (
-    <ReactModal isOpen>
-      <p>Modal content</p>
-      <button onClick={hideModal}>Hide modal</button>
-    </ReactModal>
-  ));
+  // const [showModal, hideModal] = useModal(() => (
+  //   <ReactModal isOpen>
+  //     <p>Modal content</p>
+  //     <button onClick={hideModal}>Hide modal</button>
+  //   </ReactModal>
+  //));
   return (
     <div className="App">
        <Header />
