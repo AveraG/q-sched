75c0fb4198055d92f771371f3228a53bacdeb1ae
Avera Guthrie <avera@Averas-MacBook-Pro.local>
2020-04-10 12:56:09 -0500

    removed multiple files

--- src/components/Backlog/Backlog.js
+++ src/components/Backlog/Backlog.js
@@ -1,56 +1,64 @@
-import React, {useEffect} from 'react';
+import React, { useState } from 'react';
 import './Backlog.scss';
 import ListItem from '../ListItem';
-import { toggle, handleKeyDown, updateTodo } from './AddEditDelete';
-import Tasks from './Tasks';
-
-
-
 
  function Backlog() {
-    const
+  const [todos, setTodos] = useState([
+    {
+      content: 'Feed dog',
+      isCompleted: true,
+    },
+    {
+      content: 'Do dishes',
+      isCompleted: false,
+    },
+    {
+      content: 'Complete project',
+      isCompleted: false,
+    }
+  ]);
 
-//   function handleKeyDown(e, i) {
-//     if (e.key === 'Enter') {
-//       createTodo(e, i);
-//     }
-//     if (e.key === 'Backspace' && todos[i].content === '') {
-//       e.preventDefault();
-//       return removeTodo(i);
-//     }
-//   }
+  function handleKeyDown(e, i) {
+    if (e.key === 'Enter') {
+      createTodo(e, i);
+    }
+    if (e.key === 'Backspace' && todos[i].content === '') {
+      e.preventDefault();
+      return removeTodo(i);
+    }
+  }
 
-//   function removeTodo(i) {
-//     if (i === 0 && todos.length === 1) return;
-//     setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
-//     setTimeout(() => {
-//       if(i > 0) document.forms[0].elements[i - 1].focus();
-//     }, 0);
-//   }
+  function removeTodo(i) {
+    if (i === 0 && todos.length === 1) return;
+    setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
+    setTimeout(() => {
+      if(i > 0) document.forms[0].elements[i - 1].focus();
+    }, 0);
+  }
 
-//   function createTodo(e, i) {
-//     const newTodos = [...todos];
-//     newTodos.splice(i + 1, 0, {
-//       content: '',
-//       isComlpleted: false,
-//     });
-//     setTodos(newTodos);
-//     setTimeout(() => {
-//       document.forms[0].elements[i + 1].focus();
-//     }, 0);
-//   }
+  function createTodo(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: '',
+      isComlpleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
 
-//   function updateTodo(e, i) {
-//     const newTodos = [...todos];
-//     newTodos[i].content = e.target.value;
-//     setTodos(newTodos);
-//   }
+  function updateTodo(e, i) {
+    const newTodos = [...todos];
+    newTodos[i].content = e.target.value;
+    setTodos(newTodos);
+  }
 
-//   function toggle(i) {
-//     const temporaryTodos = [...todos];
-//     temporaryTodos[i].isCompleted=!temporaryTodos[i].isCompleted;
-//     setTodos(temporaryTodos);
-//   }
+  function toggle(i) {
+    const temporaryTodos = [...todos];
+    temporaryTodos[i].isCompleted=!temporaryTodos[i].isCompleted;
+    setTodos(temporaryTodos);
+  }
 
   return (
     <div className="backlog">
